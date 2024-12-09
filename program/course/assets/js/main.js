25d2a7be2ed0dfa048df71f8189f1c420f88a88f
Max Basharin <maxbasharinweb@gmail.com>
2024-12-09 21:34:49 +0300

    Классификация типов данных в JavaScript

--- program/course/assets/js/main.js
+++ program/course/assets/js/main.js
@@ -50,3 +50,25 @@ const _apiKey = 'sdaj28hfhfh28hwfsjfsdjk';
 let userName = "John";
 let userNumber = 25;
 userNumber = 24;
+
+let number = 4.6;
+
+console.log(4/0);
+console.log('string' * 9);
+
+const persone = "Alex";
+
+const bool = false;
+
+console.log(something);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[1]);
