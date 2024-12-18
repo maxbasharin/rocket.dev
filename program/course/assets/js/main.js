a549178892cada20c4d3fedc9cb7f80b2c7260ea
Max Basharin <maxbasharinweb@gmail.com>
2024-12-18 23:38:35 +0300

    course 7

--- program/course/assets/js/main.js
+++ program/course/assets/js/main.js
@@ -60,7 +60,7 @@ const persone = "Alex";
 
 const bool = false;
 
-console.log(something);
+
 
 const obj = {
     name: "John",
@@ -72,3 +72,18 @@ console.log(obj.name);
 
 let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
 console.log(arr[1]);
+
+// alert("hello")
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18", "");
+// console.log(answer + 5)
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers)
